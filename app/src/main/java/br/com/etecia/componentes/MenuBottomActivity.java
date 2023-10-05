1bb5826f0a300c7590818fc1bd4018168be7ffb6
martinsvanessa16 <alves.nessa16@gmail.com>
2023-10-05 20:11:48 -0300

    Initial commit

---
+++ app/src/main/java/br/com/etecia/componentes/MenuBottomActivity.java
@@ -0,0 +1,14 @@
+package br.com.etecia.componentes;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MenuBottomActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.menu_bottom_layout);
+    }
+}
---
+++ app/src/main/java/br/com/etecia/componentes/MenuTopActivity.java
@@ -0,0 +1,14 @@
+package br.com.etecia.componentes;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MenuTopActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.menu_top_layout);
+    }
+}
